3811ca8252e28d5c6752b6ff1a00c59e1e8e1086
hiroyuki0210 <h.tamaki@atomitech.jp>
2020-01-16 18:00:45 +0900

    Finish user signup

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,8 @@
 class User < ApplicationRecord
      before_save {self.email = email.downcase}
 
-     VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
-     validates :name, presence: true, length: {maximum:10}
+     VALID_EMAIL_REGEX = /[\w+\-.]+@[a-z\d\-.]+\.[a-z]/i
+     validates :name, presence: true, length: {maximum:30}
      validates :email, presence: true, format: {with: VALID_EMAIL_REGEX}, uniqueness: {case_sensitive:false}
      validates :password, presence: true, length:{minimum: 6}
 
