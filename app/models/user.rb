cf27dae470065483c09719876ee7332dfd92281a
hiroyuki0210 <h.tamaki@atomitech.jp>
2020-01-20 18:09:51 +0900

    add session function

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ApplicationRecord
+     attr_accessor :remember_token
+
      before_save {self.email = email.downcase}
 
      VALID_EMAIL_REGEX = /[\w+\-.]+@[a-z\d\-.]+\.[a-z]/i
@@ -11,6 +13,28 @@ class User < ApplicationRecord
      # 渡された文字列のハッシュ値を返す
      def User.digest(string)
           cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
-                                                        BCrypt::Engine.costBCrypt::Password.create(string, cost: cost)
-   end
+                                                        BCrypt::Engine.cost
+          BCrypt::Password.create(string, cost: cost)
+     end
+
+     # ランダムなトークンを返す
+     def User.new_token
+          SecureRandom.urlsafe_base64
+     end
+
+     # 永続セッションのためユーザをデータベースに記憶する
+     def remember
+          self.remember_token = User.new_token
+          update_attribute(:remember_digest, User.digest(remember_token))
+     end
+
+     # 渡されたトークンがダイジェストと一致したらtrueを返す
+     def authenticated?(remember_token)
+          BCrypt::Password.new(remember_digest).is_password?(remember_token)
+     end
+
+     # ユーザのログイン情報を破棄する
+     def forget
+          update_attribute(:remember_digest, nil)
+     end
 end
