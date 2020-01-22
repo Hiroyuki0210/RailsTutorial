4840a25798627c25a9a178a32fa54e42bda8e9c7
hiroyuki0210 <h.tamaki@atomitech.jp>
2020-01-22 15:04:44 +0900

    Finish user edit, update, index, and destroy actions

--- Gemfile
+++ Gemfile
@@ -11,6 +11,10 @@ gem 'jbuilder',     '2.6.4'
 gem 'bootsnap', '>= 1.1.0', require: false
 gem 'bootstrap-sass', '3.3.7'
 gem 'bcrypt',         '3.1.12'
+gem 'faker',          '1.7.3'
+gem 'will_paginate',           '3.1.6'
+gem 'bootstrap-will_paginate', '1.0.0'
+
 
 group :development, :test do
   gem 'sqlite3',      '1.3.13'
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,14 @@
 class UsersController < ApplicationController
-  before_action :logged_in_user, only: [:edit, :update]
+  before_action :logged_in_user, only: [:index, :edit, :update, :destroy]
   before_action :correct_user, only: [:edit, :update]
+  before_action :admin_user, only: [:destory]
 
   def new
     @user = User.new
+  end
+
+  def index
+    @users = User.paginate(page: params[:page])
   end
 
   def show
@@ -36,7 +41,14 @@ class UsersController < ApplicationController
     end
   end
 
+  def destroy
+    User.find(params[:id]).destroy
+    flash[:success] = "User deleted"
+    redirect_to users_url
+  end
+
   private
+    # Strong Parameters
     def user_params
       params.require(:user).permit(:name, :email, :password, :password_confirmation)
     end
@@ -54,6 +66,11 @@ class UsersController < ApplicationController
     def correct_user
       @user = User.find(params[:id])
       redirect_to (root_url) unless current_user?(@user)
+    end
+
+    # 管理者でなければルートページに戻す
+    def admin_user
+      redirect_to(root_url) unless current_user.admin?
     end
 
 end
