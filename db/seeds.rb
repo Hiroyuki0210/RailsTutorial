da225f28ddfffe8d897339e5fc0448de66f8b2f7
hiroyuki0210 <h.tamaki@atomitech.jp>
2020-01-29 18:12:05 +0900

    follow

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,6 +6,7 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
+# サンプル管理人
 User.create!(name: "Example User",
              email: "example@railstutorial.org",
              password:              "foobar",
@@ -14,6 +15,7 @@ User.create!(name: "Example User",
              activated: true,
              activated_at: Time.zone.now)
 
+# サンプルユーザ
 99.times do |n|
      name = Faker::Name.name
      email = "example-#{n+1}@railstutorial.org"
@@ -26,8 +28,17 @@ User.create!(name: "Example User",
                   activated_at: Time.zone.now)
 end
 
+# サンプルマイクロポスト
 users = User.order(:created_at).take(6)
 50.times do
      content = Faker::Lorem.sentence(5)
      users.each { |user| user.microposts.create!(content: content)}
 end
+
+# サンプルフォロー
+users = User.all
+user = users.first
+following = users[2..50]
+followers = users[3..50]
+following.each {|followed| user.follow(followed)}
+followers.each {|follower| follower.follow(user)}
